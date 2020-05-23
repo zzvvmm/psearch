0d820f96ef44b7ff8748db661e6a429715ae37c7
zzvvmm <zzvvmm96@gmail.com>
2020-05-23 18:04:42 +0700

    first deploy version

--- resources/views/images/summary.blade.php
+++ resources/views/images/summary.blade.php
@@ -2,13 +2,13 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Gắn mô tả cho ảnh</title>
-<link href="../css/css.css" rel="stylesheet" type="text/css" />
-<link rel="shortcut icon" href="../favicon.ico" />
+<link href="{{asset('/css/css.css')}}" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="{{asset('/favicon.ico')}}" />
 <meta name="theme-color" content="#444" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="{{asset('js/bootstrap.min.js')}}" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 <style>
 thead {color:green;}
@@ -32,7 +32,7 @@ table, th, td, tr {
   <div class="margin">
       <div class="logo-outer">
         <a href="{{ route('home')}}">
-          <img src="images/hust.png" style="height: 70px"/>
+          <img src="{{asset('images/hust.png')}}" style="height: 70px"/>
         </a>
        </div>
     </div>
@@ -57,7 +57,7 @@ table, th, td, tr {
       Câu truy vấn có ID lớn nhất đã được nhập: {{$last_labeled}} (<a href="{{route('labels.index', ['page' => ceil($last_labeled/20)])}}">Đi đến</a>)
     </li>
     <li>
-      ID các ảnh bị thiếu
+      ID các ảnh bị thiếu:
       <ul>
         @foreach ($not_edited_images as $img)
           <li>
